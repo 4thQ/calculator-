6ddc123d0f836b279c0b94a8742175c0a7e2ff43
4thQ <164560954+4thQ@users.noreply.github.com>
2024-11-11 16:29:45 +0000

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -43,6 +43,7 @@ const span0 = document.querySelector(".span0");
 btnContainer.addEventListener("click", (e) => {
   if (e.target.classList.contains("num")) {
     const btnValue = e.target.innerText;
+
     num1 += btnValue;
     span0.innerText = num1;
     console.log(num1);
@@ -53,6 +54,32 @@ btnContainer.addEventListener("click", (e) => {
     operator = e.target.innerText;
     console.log(operator);
 
-    span0.innerText = firstNumber + operator + secondNumber;
+    span0.innerText = firstNumber + operator;
   }
+
+  if (e.target.classList.contains("eq")) {
+    let firstNumber1 = Number(firstNumber);
+
+    let num11 = Number(num1);
+
+    sumOfNums = operate(firstNumber1, operator, num11);
+
+    span0.innerText = sumOfNums;
+
+    console.log(sumOfNums);
+  } else if (e.target.classList.contains("clear")) {
+    span0.innerHTML = "";
+    firstNumber = "";
+    operator = "";
+    num1 = "";
+  }
+
+  console.log("-----");
+  console.log(firstNumber);
+  console.log(operator);
+  console.log(num1);
 });
+
+// firstnumber has the first Number
+// operator has the operator
+// num1 hold the second Number.
